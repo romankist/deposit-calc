d3aab72fabd38e4278d734b7073d4355e2b7418c
Roman <roman@kistanov.com>
2017-02-15 09:14:28 +0700

    Implemented input analys

--- vklad.c
+++ vklad.c
@@ -11,15 +11,26 @@ int main()
     printf("Insert days:");
     scanf("%d",&days);
 
-    if (days>365) {
-	printf("Days must be <365");
+    if ((days>365)||(days<0)) {
+	printf("Days must be between 0-365");
 	return 1;
     }
     if (money<10000) {
 	printf("Money must be >10000");
 	return 1;
     }
-    
+    if (money<100000) {
+	if (days<=30) money=money-(money*0.1);
+	if ((days>=31)&&(days<=120)) money=money+(money*0.02);
+	if ((days>=121)&&(days<=240)) money=money+(money*0.06);
+	if ((days>=241)&&(days<=365)) money=money+(money*0.12);
+    }
+    if (money>=100000) {
+	if (days<=30) money=money-(money*0.1);
+	if ((days>=31)&&(days<=120)) money=money+(money*0.03);
+	if ((days>=121)&&(days<=240)) money=money+(money*0.08);
+	if ((days>=241)&&(days<=365)) money=money+(money*0.15);
+    }
 
 
 
